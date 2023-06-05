a00df75603f1be32637c9b1f0834787b1b061edb
siyamanga22 <zwenisiyamanga@gmail.com>
2023-06-05 16:51:58 +0200

    unfinished

--- script/code.js
+++ script/code.js
@@ -1,9 +1,9 @@
 let inputName1 = document.querySelector('#inputName1');
-// let sort =document.querySelector('#sort');
 let addList = document.querySelector('#addList');
 let output=document.querySelector('#todo');
 let Arr= [];
-
+let deleteButtons = [];
+// ADD BUTTON
 addList.addEventListener("click",(event)=>{
     event.preventDefault()
     if(inputName1.value){
@@ -18,29 +18,77 @@ addList.addEventListener("click",(event)=>{
     output.innerHTML= "";
     Arr.forEach((name)=> {
         output.innerHTML += `
-        <li>
-        <input type="checkbox" onclick="toggleLineThrough(this)">${name}</li>
-        `
+        <ul>
+        <input type="checkbox" onclick="toggleLineThrough(this)">
+        <p class="itemName">${name}</p> 
+        <button class= "edit" id="EDIT">Edit</button><button class= "deleteButton" id="THEBUT">X</button>
+        </ul>`
 
-    })
+        deleteButtons = [...document.querySelectorAll('.deleteButton')];
+        deleteButtons.forEach((button) => {
+          button.addEventListener('click', function () {
+          console.log(event.target )
+            let listItem = [...document.querySelectorAll('.itemName')];
+        
+            listItem.forEach(item => {
+        
+              console.log(listItem)
+              // listItem.remove();
+          
+             
+              let index =Arr.indexOf(item.textContent);
+              console.log(Arr.indexOf(item.textContent))
+              if(index != -1){
+                console.log('Hello')
+                console.log(typeof index)
+                Arr.splice(index,1);
+        
+                output.innerHTML = '';
+                Arr.forEach((name)=> {
+                  output.innerHTML += `
+                  <ul>
+                  <input type="checkbox" onclick="toggleLineThrough(this)">
+                  <p class="itemName">${name}</p> 
+                  <button class= "edit" id="EDIT">Edit</button><button class= "deleteButton" id="THEBUT">X</button>
+                  </ul>`
+          
+              })
+              console.log(Arr)
+              } else {
+                console.log('Hey')
+              }
+            })
+            // localStorage.setItem("names", JSON.stringify(Arr));
+          });
+        });
+    });
+
 })
 
+
+// checked function
 function toggleLineThrough(element) {
     if (element.checked) {
-      document.getElementById("todo").style.textDecoration = "line-through";
+      element.parentNode.style.textDecoration="line-through"
+      // document.getElementById("todo").style.textDecoration = "line-through";
     }
     else {
-      document.getElementById("todo").style.textDecoration = "none";
+      element.parentNode.style.textDecoration="line-through"
+      // document.getElementById("todo").style.textDecoration = "none";
     }
 }
-
+// sort function
 function renderTodoList() {
     Arr.sort(); // Sort the array alphabetically
     output.innerHTML = Arr.map(name => `
-      <li>
+      <ul>
         <input type="checkbox" onclick="toggleLineThrough(this)">
-        ${name}
-      </li>
-    `).join("");
+        ${name}<button class= "deleteButton" id="THEBUT">X</button>
+      </ul>
+      `).join("");
   }
+
+
+
+
 
